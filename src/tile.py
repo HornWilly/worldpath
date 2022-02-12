46aabd52a5fc788ea080123f4304a5cc49c68703
HornWilly <mehdi.maache@gmail.com>
2022-02-12 23:52:27 +0100

    Modify hitbox from tiles and player and check hitbox for collision

--- src/tile.py
+++ src/tile.py
@@ -6,3 +6,7 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+        # Returns a new rectangle with the size changed by the given offset.
+        # The rectangle remains centered around its current center.
+        # Negative values will shrink the rectangle.
+        self.hitbox = self.rect.inflate(0, -10)
