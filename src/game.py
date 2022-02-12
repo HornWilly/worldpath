712d03864fe01669c4c364b4453285f896eefb8a
HornWilly <mehdi.maache@gmail.com>
2022-02-12 19:17:48 +0100

    Init project game world path

---
+++ src/game.py
@@ -0,0 +1,27 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('World Path')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
