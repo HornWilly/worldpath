e913acf5e75bbbd53db91a24964bb50d149b5898
HornWilly <mehdi.maache@gmail.com>
2022-02-14 19:18:30 +0100

    Use new map large objects and set a new hitbox for these objects

--- src/tile.py
+++ src/tile.py
@@ -9,9 +9,11 @@ class Tile(pygame.sprite.Sprite):
         self.image = surface
         if sprite_type == 'object':
             self.rect = self.image.get_rect(topleft=(pos[0], pos[1] - TILESIZE))
+            self.hitbox = self.rect.copy()
+            self.hitbox.update(self.rect.left, self.rect.top + 34, self.rect.width, self.rect.height - 44)
         else:
             self.rect = self.image.get_rect(topleft=pos)
-        # Returns a new rectangle with the size changed by the given offset.
-        # The rectangle remains centered around its current center.
-        # Negative values will shrink the rectangle.
-        self.hitbox = self.rect.inflate(0, -10)
+            # Returns a new rectangle with the size changed by the given offset.
+            # The rectangle remains centered around its current center.
+            # Negative values will shrink the rectangle.
+            self.hitbox = self.rect.inflate(0, -10)
