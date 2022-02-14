25cac32925e83e739eb7af60ab004dc49fbe786c
Mehdi Maache <mehdi.maache@gmail.com>
2022-02-14 16:36:34 +0100

    set grass, objects and boundary

--- src/level.py
+++ src/level.py
@@ -1,9 +1,12 @@
+from random import choice
+
 import pygame
 from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
 from camera import Camera
+from utils import *
 
 
 class Level:
@@ -18,14 +21,31 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        layouts = {
+            'boundary': get_layout_from_csv('../map/map_floor_blocks.csv'),
+            'grass': get_layout_from_csv('../map/map_grass.csv'),
+            'objects': get_layout_from_csv('../map/map_objects.csv'),
+        }
+        graphics = {
+            'grass': get_files_list_from_folder('../graphics/grass'),
+            'objects': get_files_list_from_folder('../graphics/objects')
+        }
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+                        if style == 'objects':
+                            object_image = graphics['objects'][int(col)]
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', object_image)
+
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
