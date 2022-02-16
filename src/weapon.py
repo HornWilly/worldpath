f4ac5881efc04e7c3d43745e5c1346d9e335a94c
HornWilly <mehdi.maache@gmail.com>
2022-02-16 23:25:42 +0100

    Display weapon on right of the player when player is moving on right

--- src/weapon.py
+++ src/weapon.py
@@ -4,5 +4,15 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
+        # 0 => up
+        # 1 => down
+        # 2 => left
+        # 3 => right
+        direction = player.status_movement
+
         self.image = pygame.Surface((40, 40))
-        self.rect = self.image.get_rect(center=player.rect.center)
+
+        if direction == 3:
+            self.rect = self.image.get_rect(midleft=player.rect.midright)
+        else:
+            self.rect = self.image.get_rect(center=player.rect.center)
