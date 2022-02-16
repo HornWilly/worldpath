c157f221b1e8635f33603dbad189043a765813ac
HornWilly <mehdi.maache@gmail.com>
2022-02-17 00:28:01 +0100

    Change weapon on backspace and add antispam to change weapon every 200ms

--- src/weapon.py
+++ src/weapon.py
@@ -10,7 +10,7 @@ class Weapon(pygame.sprite.Sprite):
         # 3 => right
         direction = player.status_movement
 
-        image_path = f'../graphics/weapons/{player.weapon}/{player.status_to_animation[direction]}.png'
+        image_path = f'../graphics/weapons/{player.get_weapon()}/{player.status_to_animation[direction]}.png'
         self.image = pygame.image.load(image_path).convert_alpha()
 
         # pygame.math.Vector2(x, y) -> We want weapon to be exactly where player's arm is
