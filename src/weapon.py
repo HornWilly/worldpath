3d3981b3d0acbc43de5720be9c5e30665207195c
Mehdi Maache <mehdi.maache@gmail.com>
2022-02-17 10:55:10 +0100

    Use string to check direction

--- src/weapon.py
+++ src/weapon.py
@@ -4,21 +4,17 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
-        # 0 => up
-        # 1 => down
-        # 2 => left
-        # 3 => right
-        direction = player.status_movement
+        direction = player.status_to_animation[player.status_movement]
 
-        image_path = f'../graphics/weapons/{player.get_weapon()}/{player.status_to_animation[direction]}.png'
+        image_path = f'../graphics/weapons/{player.get_weapon()}/{direction}.png'
         self.image = pygame.image.load(image_path).convert_alpha()
 
         # pygame.math.Vector2(x, y) -> We want weapon to be exactly where player's arm is
-        if direction == 0:
+        if direction == "up":
             self.rect = self.image.get_rect(midbottom=player.rect.midtop + pygame.math.Vector2(-10, 0))
-        elif direction == 1:
+        elif direction == "down":
             self.rect = self.image.get_rect(midtop=player.rect.midbottom + pygame.math.Vector2(-10, 0))
-        elif direction == 2:
+        elif direction == "left":
             self.rect = self.image.get_rect(midright=player.rect.midleft + pygame.math.Vector2(0, 16))
-        else:
+        else:  # right
             self.rect = self.image.get_rect(midleft=player.rect.midright + pygame.math.Vector2(0, 16))
